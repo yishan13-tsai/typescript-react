d3ee5761a72a541d05311f92ab37432c30c4b263
Tim Ji <timji@kktv.me>
2024-01-24 00:07:45 +0800

    antd init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import {Button} from "antd";
+
 const App = () => (
     <>
-        <h1 className="text-3xl font-bold underline">
+        <h1 className="text-3xl underline">
             Hello world!
         </h1>
+        <Button type="primary">Button</Button>
     </>
 );
 
