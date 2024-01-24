5ecce0abdb003fb7b1d588eb80fd561ed6531ce0
esther 13 <yishan13.tsai@gmail.com>
2024-01-24 10:10:54 +0800

    cicd: fix build baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 export default defineConfig({
-  base: isProd ? '/typescript-react/' : '/',
+  base: '/',
   plugins: [react()],
 })
