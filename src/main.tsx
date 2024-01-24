a49441e5c4145cbf092b3ff769192d90fe631e92
Tim Ji <timji@kktv.me>
2024-01-24 09:17:13 +0800

    add some route page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import './index.css'
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/about",
+        element: <h1>About</h1>,
+    },
+    {
+        path: "/users",
+        element: <h1>Users</h1>,
+    },
+    {
+        path: "*",
+        element: <h1>Not Found</h1>,
+    },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )
