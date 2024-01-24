cb8a61703fe573aebe0454ff34da317c67e1687a
yishan tsai <yishan13.tsai@gmail.com>
2024-01-25 00:44:05 +0800

    fix: typo fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ const App = () => (
         </h1>
         <p className="text-xl">字體大小示範: test-xl</p>
         <p className="text-lg">字體大小示範: text-lg</p>
-        <p className="leading-heading">字體ling height示範: leading-heading</p>
-        <p className="leading-normal">字體ling height示範: leading-normal</p>
+        <p className="leading-heading">字體line height示範: leading-heading</p>
+        <p className="leading-normal">字體line height示範: leading-normal</p>
         <p className="tracking-heading">字體letter spacing示範: tracking-heading</p>
         <p className="tracking-normal">字體letter spacing示範: tracking-normal</p>
         <p className="text-primary-100">字體顏色示範: color-primary-100</p>
