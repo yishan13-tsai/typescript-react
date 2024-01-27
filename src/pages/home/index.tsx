6e1195141946a35e8d23b8bd2655d5d3cf547b48
Tim Ji <timji@kktv.me>
2024-01-28 05:29:20 +0800

    init axios and swr, add basic example

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,38 @@
 import {PrimaryButton} from "./primaryButton.tsx";
+import {useState} from "react";
+import axios from "axios";
+import useSWR from "swr";
 
-const Index = () => (
-  <>
-    <h1 className="text-3xl underline">
-      測試字體
-    </h1>
-    <p className="text-xl">字體大小示範: test-xl</p>
-    <p className="text-lg">字體大小示範: text-lg</p>
-    <p className="leading-heading">字體line height示範: leading-heading</p>
-    <p className="leading-normal">字體line height示範: leading-normal</p>
-    <p className="tracking-heading">字體letter spacing示範: tracking-heading</p>
-    <p className="tracking-normal">字體letter spacing示範: tracking-normal</p>
-    <p className="text-primary-100">字體顏色示範: color-primary-100</p>
-    <p className="bg-info-20">背景顏色示範: bg-info-20</p>
-    <PrimaryButton>Button</PrimaryButton>
-  </>
-);
+const Index = () => {
+  const [canFetch, setCanFetch] = useState(false);
+  const fetcher = async (url: string) => {
+    return axios.get(url).then((response) => {
+      console.log(response);
+      return response.data;
+    })
+  }
+
+  const {data, error, isLoading} = useSWR(canFetch ? "https://jsonplaceholder.typicode.com/todos/1" : null, fetcher)
+
+  return (
+    <>
+      <h1 className="text-3xl underline">
+        測試字體
+      </h1>
+      <p className="text-xl">字體大小示範: test-xl</p>
+      <p className="text-lg">字體大小示範: text-lg</p>
+      <p className="leading-heading">字體line height示範: leading-heading</p>
+      <p className="leading-normal">字體line height示範: leading-normal</p>
+      <p className="tracking-heading">字體letter spacing示範: tracking-heading</p>
+      <p className="tracking-normal">字體letter spacing示範: tracking-normal</p>
+      <p className="text-primary-100">字體顏色示範: color-primary-100</p>
+      <p className="bg-info-20">背景顏色示範: bg-info-20</p>
+      <PrimaryButton
+        onClick={() => setCanFetch(true)}
+        loading={isLoading}
+      >Button</PrimaryButton>
+      {data ? <p>{data.title}</p> : null}
+    </>
+  );
+}
 export default Index;
