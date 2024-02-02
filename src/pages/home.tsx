f00a02cfe4449a27caf8c9f2724bcf2fe55c8839
Tim Ji <timji@kktv.me>
2024-02-03 00:41:42 +0800

    reformat src/pages/home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,22 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 import FoodSlider from '../component/home/FoodSlider'
 import './home.css'
-import { NewsType } from '@/types/news.model';
-import axios from '@/utils/axios';
-import useSWR from 'swr';
+import { NewsType } from '@/types/news.model'
+import axios from '@/utils/axios'
+import useSWR from 'swr'
 
 const axiosGet = async (url: string) => {
   return axios.get(url).then((response) => {
     return response
   })
 }
+
 function home() {
   const [canFetch, setCanFetch] = useState<boolean>(false)
   const [news, setNews] = useState<NewsType[]>([])
-  const { data, error } = useSWR(
-    canFetch ? `/home/news` : null,
-    axiosGet,
-  )
+  const { data, error } = useSWR(canFetch ? `/home/news` : null, axiosGet)
   useEffect(() => {
     if (news) {
       setCanFetch(true)
@@ -33,13 +31,14 @@ function home() {
     <>
       <div className="">
         <section className="w-full h-[1080px] bg-blue-800 flex justify-center">
-          <div className="
+          <div
+            className="
             h-full w-full
             bg-[url('./home_page/hero/hero_img.jpg')]
             bg-no-repeat object-center bg-cover
             bg-[#00000099]
-            bg-blend-multiply">
-          </div>
+            bg-blend-multiply"
+          ></div>
           <div className="container mt-[14.75rem] p-[0.5rem] absolute top-0">
             <div className="grid grid-cols-2 relative justify-center items-center h-[678px]">
               <div className="left_col_block">
@@ -49,20 +48,30 @@ function home() {
                 </div>
               </div>
               <div className="right_col_block h-full">
-                <div className="bg h-full w-full
+                <div
+                  className="bg h-full w-full
                   border border-solid border-neutral-0 border-l-0 border-b-0 rounded-[80px]
-                  backdrop-filter backdrop-blur-sm"></div>
+                  backdrop-filter backdrop-blur-sm"
+                ></div>
                 <div className="main_block absolute top-1/2 right-[9.5%] -translate-y-1/2">
                   <div className="main_title text-neutral-0">
                     <div>
                       <h1 className="m-0 text-[6.25rem] font-bold">高雄</h1>
-                      <h1 className="m-0 text-[6.25rem] font-bold">豪華住宿之選</h1>
+                      <h1 className="m-0 text-[6.25rem] font-bold">
+                        豪華住宿之選
+                      </h1>
                     </div>
-                    <p className="m-0 text-[2rem] font-semibold mb-[60px]">我們致力於為您提供無與倫比的奢華體驗與優質服務</p>
+                    <p className="m-0 text-[2rem] font-semibold mb-[60px]">
+                      我們致力於為您提供無與倫比的奢華體驗與優質服務
+                    </p>
                   </div>
-                  <div className="btn h-[116px] w-full bg-neutral-0 rounded-lg
-                    flex justify-end items-center">
-                    <p className="m-0 btn_title relative mr-52 font-bold text-[24px] mb-0">立即訂房</p>
+                  <div
+                    className="btn h-[116px] w-full bg-neutral-0 rounded-lg
+                    flex justify-end items-center"
+                  >
+                    <p className="m-0 btn_title relative mr-52 font-bold text-[24px] mb-0">
+                      立即訂房
+                    </p>
                   </div>
                 </div>
               </div>
@@ -70,7 +79,11 @@ function home() {
           </div>
         </section>
         <section className="news w-[70%] mx-auto my-[120px] px-4 bg-primary-10 relative">
-          <img className="absolute right-[-80px] top-[20px] w-[12%] bg-cover" src="./home_page/dot.png" alt="" />
+          <img
+            className="absolute right-[-80px] top-[20px] w-[12%] bg-cover"
+            src="./home_page/dot.png"
+            alt=""
+          />
           <div className="-mx-4 flex flex-wrap">
             <div className="right_col w-2/12 flex-none">
               <div className="subtitle font-bold text-5xl text-primary-100">
@@ -80,16 +93,20 @@ function home() {
             </div>
             <div className="w-10/12 flex-none">
               {news.map((item: NewsType) => {
-                return <div className="flex flex-wrap mb-10">
-                  <div className="-ml-6 pr-6 w-5/12 flex-none">
-                    {/* <img className="w-full object-cover" src="./home_page/news/news_01.png" /> */}
-                    <img className="w-full object-cover" src={item.image} />
+                return (
+                  <div className="flex flex-wrap mb-10">
+                    <div className="-ml-6 pr-6 w-5/12 flex-none">
+                      {/* <img className="w-full object-cover" src="./home_page/news/news_01.png" /> */}
+                      <img className="w-full object-cover" src={item.image} />
+                    </div>
+                    <div className="-mx-4 w-7/12 flex-none px-4 grid content-center">
+                      <p className="font-bold text-[32px]">{item.title}</p>
+                      <p className="font-medium text-base text-neutral-80">
+                        {item.description}
+                      </p>
+                    </div>
                   </div>
-                  <div className="-mx-4 w-7/12 flex-none px-4 grid content-center">
-                    <p className="font-bold text-[32px]">{item.title}</p>
-                    <p className="font-medium text-base text-neutral-80">{item.description}</p>
-                  </div>
-                </div>
+                )
               })}
               {/* <div className="flex flex-wrap mb-10">
                 <div className="-ml-6 pr-6 w-5/12 flex-none">
@@ -120,27 +137,38 @@ function home() {
               </div> */}
             </div>
           </div>
-          <img className="absolute left-[-80px] bottom-[-180px] z-10 w-[12%] bg-cover" src="./home_page/dot.png" alt="" />
+          <img
+            className="absolute left-[-80px] bottom-[-180px] z-10 w-[12%] bg-cover"
+            src="./home_page/dot.png"
+            alt=""
+          />
         </section>
         <section className="about w-full bg-neutral-120 relative">
           <div>
-            <img className="w-full object-cover mt-[120px] mb-[200px]" src="./home_page/about/about_bg.png" />
-            <div className="container h-[672px] w-2/3 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+            <img
+              className="w-full object-cover mt-[120px] mb-[200px]"
+              src="./home_page/about/about_bg.png"
+            />
+            <div
+              className="container h-[672px] w-2/3 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
                     border border-solid border-neutral-0 border-l-0 border-b-0 rounded-[80px] rounded-br-none
                     bg-gradient-to-b from-[#140F0ACC] to-[#BE9C7CCC]
                     backdrop-filter backdrop-blur-sm
-                    flex justify-center items-center">
+                    flex justify-center items-center"
+            >
               <div className="m-20 text-neutral-0">
                 <div className="about_title font-bold text-5xl mb-20 relative">
                   <p className="m-0 leading-[57.6px] tracking-heading">關於</p>
                   <p className="m-0 leading-[57.6px] tracking-heading">我們</p>
                 </div>
                 <p className="mb-10">
-                  享樂酒店，位於美麗島高雄的心臟地帶，是這座城市的璀璨瑰寶與傲人地標。<br />
+                  享樂酒店，位於美麗島高雄的心臟地帶，是這座城市的璀璨瑰寶與傲人地標。
+                  <br />
                   我們的存在，不僅僅是為了提供奢華的住宿體驗，更是為了將高雄的美麗與活力，獻給每一位蒞臨的旅客。
                 </p>
                 <p className="mb-10">
-                  我們的酒店，擁有時尚典雅的裝潢，每一個細節都充滿著藝術與設計的精緻。<br />
+                  我們的酒店，擁有時尚典雅的裝潢，每一個細節都充滿著藝術與設計的精緻。
+                  <br />
                   我們的員工，都以熱情的服務與專業的態度，讓每一位客人都能感受到賓至如歸的溫暖。
                 </p>
                 <p className="mb-10">
@@ -155,26 +183,55 @@ function home() {
           </div>
         </section>
         <section className="rooms bg-neutral-120 pb-[120px] relative">
-          <img className="absolute right-[5px] top-[100px] w-[52%] bg-cover" src="./home_page/line.png" alt="" />
-          <img className="absolute right-[5px] bottom-[0px] w-full bg-cover" src="./home_page/flow_bg.png" alt="" />
+          <img
+            className="absolute right-[5px] top-[100px] w-[52%] bg-cover"
+            src="./home_page/line.png"
+            alt=""
+          />
+          <img
+            className="absolute right-[5px] bottom-[0px] w-full bg-cover"
+            src="./home_page/flow_bg.png"
+            alt=""
+          />
           <div className="grid grid-cols-2 gap-20">
             <div className="left_col_block w-full">
-              <img className="w-full object-cover" src="./home_page/rooms/rooms_01.png" />
+              <img
+                className="w-full object-cover"
+                src="./home_page/rooms/rooms_01.png"
+              />
             </div>
             <div className="right_col_block w-2/3 flex flex-col justify-end relative -top-24">
-              <p className="m-0 mb-4 font-bold text-neutral-0 text-[40px]">尊爵雙人房</p>
-              <p className="m-0 mb-10 font-medium text-neutral-0 text-base">享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。</p>
-              <p className="m-0 mb-10 font-medium text-neutral-0 text-[32px]">NT$ 10,000</p>
-              <div className="btn h-[116px] w-full bg-neutral-0 rounded-lg
-                  flex justify-end items-center">
-                <p className="m-0 btn_title relative mr-52 font-bold text-[24px] mb-0">查看更多</p>
+              <p className="m-0 mb-4 font-bold text-neutral-0 text-[40px]">
+                尊爵雙人房
+              </p>
+              <p className="m-0 mb-10 font-medium text-neutral-0 text-base">
+                享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。
+              </p>
+              <p className="m-0 mb-10 font-medium text-neutral-0 text-[32px]">
+                NT$ 10,000
+              </p>
+              <div
+                className="btn h-[116px] w-full bg-neutral-0 rounded-lg
+                  flex justify-end items-center"
+              >
+                <p className="m-0 btn_title relative mr-52 font-bold text-[24px] mb-0">
+                  查看更多
+                </p>
               </div>
             </div>
           </div>
         </section>
         <section className="food w-[70%] mx-auto my-[120px] px-4 bg-primary-10 relative">
-          <img className="absolute right-[-120px] top-[-146px] w-[12%] bg-cover" src="./home_page/dot.png" alt="" />
-          <img className="absolute left-[-220px] top-[-60px] w-[13.5%] bg-cover" src="./home_page/straight_line.png" alt="" />
+          <img
+            className="absolute right-[-120px] top-[-146px] w-[12%] bg-cover"
+            src="./home_page/dot.png"
+            alt=""
+          />
+          <img
+            className="absolute left-[-220px] top-[-60px] w-[13.5%] bg-cover"
+            src="./home_page/straight_line.png"
+            alt=""
+          />
           <div className="subtitle_line_right font-bold text-5xl text-primary-100 mb-20">
             <p className="m-0 leading-[57.6px] tracking-heading">佳餚</p>
             <p className="m-0 leading-[57.6px] tracking-heading">美饌</p>
@@ -189,43 +246,69 @@ function home() {
             </div>
             <div className="w-full mb-10">
               <p className="text-neutral-0">台灣高雄市新興區六角路123號</p>
-              <img className="w-full object-cover" src="./home_page/transportation/transportation_map.png" alt="" />
+              <img
+                className="w-full object-cover"
+                src="./home_page/transportation/transportation_map.png"
+                alt=""
+              />
             </div>
             <div className="w-full grid grid-cols-3 content-center gap-6">
               <div>
                 <div className="w-[80px] h-[80px]">
-                  <img className="w-full object-cover" src="./home_page/transportation/transportation_icon_car.png" alt="" />
+                  <img
+                    className="w-full object-cover"
+                    src="./home_page/transportation/transportation_icon_car.png"
+                    alt=""
+                  />
                 </div>
                 <div className="text-neutral-0">
                   <p>自行開車</p>
-                  <p>如果您選擇自行開車，可以透過國道一號下高雄交流道，往市區方向行駛，並依路標指示即可抵達「享樂酒店」。飯店內設有停車場，讓您停車方便。</p>
+                  <p>
+                    如果您選擇自行開車，可以透過國道一號下高雄交流道，往市區方向行駛，並依路標指示即可抵達「享樂酒店」。飯店內設有停車場，讓您停車方便。
+                  </p>
                 </div>
               </div>
               <div>
                 <div className="w-[80px] h-[80px]">
-                  <img className="w-full object-cover" src="./home_page/transportation/transportation_icon_train.png" alt="" />
+                  <img
+                    className="w-full object-cover"
+                    src="./home_page/transportation/transportation_icon_train.png"
+                    alt=""
+                  />
                 </div>
                 <div className="text-neutral-0">
                   <p>高鐵/火車</p>
-                  <p>如果您是搭乘高鐵或火車，可於左營站下車，外頭有計程車站，搭乘計程車約20分鐘即可抵達。或者您也可以轉乘捷運紅線至中央公園站下車，步行約10分鐘便可抵達。</p>
+                  <p>
+                    如果您是搭乘高鐵或火車，可於左營站下車，外頭有計程車站，搭乘計程車約20分鐘即可抵達。或者您也可以轉乘捷運紅線至中央公園站下車，步行約10分鐘便可抵達。
+                  </p>
                 </div>
               </div>
               <div>
                 <div className="w-[80px] h-[80px]">
-                  <img className="w-full object-cover" src="./home_page/transportation/transportation_icon_luxurycar.png" alt="" />
+                  <img
+                    className="w-full object-cover"
+                    src="./home_page/transportation/transportation_icon_luxurycar.png"
+                    alt=""
+                  />
                 </div>
                 <div className="text-neutral-0">
                   <p>禮賓車服務</p>
-                  <p>承億酒店提供禮賓專車接送服務，但因目的地遠近會有不同的收費，請撥打電話將由專人為您服務洽詢專線：(07)123-4567</p>
+                  <p>
+                    承億酒店提供禮賓專車接送服務，但因目的地遠近會有不同的收費，請撥打電話將由專人為您服務洽詢專線：(07)123-4567
+                  </p>
                 </div>
               </div>
             </div>
-            <img className="absolute bottom-0 left-0 w-full bg-cover" src="./home_page/full_line.png" alt="" />
+            <img
+              className="absolute bottom-0 left-0 w-full bg-cover"
+              src="./home_page/full_line.png"
+              alt=""
+            />
           </div>
         </section>
       </div>
     </>
-  );
+  )
 }
 
-export default home;
+export default home
