a5f8bd2fd234f3955428185aefd3e2b89e01dc11
hsuchiashih <hsuchiashih@gmail.com>
2024-02-02 20:45:24 +0800

    loginApi

---
+++ src/pages/login/NoticeModal.tsx
@@ -0,0 +1,16 @@
+import { Modal } from 'antd'
+
+const NoticeModal = ({ isOpen = false, message= '通知訊息'}) => {
+  return (
+    <Modal footer={null} width="50%" closeIcon={false} open={isOpen}>
+      <div className="h-[75vh] flex justify-center items-center flex-col">
+        <img src="./logo-primary.png" alt="logo-primary" />
+        <p className="text-xl font-bold text-neutral-100 mt-4">
+          { message }
+        </p>
+      </div>
+    </Modal>
+  )
+}
+
+export default NoticeModal
