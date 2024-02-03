2ec045a89c842584c0deb780e006648d9895685e
Tim Ji <timji@kktv.me>
2024-02-04 04:17:00 +0800

    refactor

--- src/pages/account/UserInfo.tsx
+++ src/pages/account/UserInfo.tsx
@@ -1,13 +1,18 @@
-import { Card, Col, Flex } from 'antd'
+import { Card, Col, Flex, Form } from 'antd'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/store.ts'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import UserInfoForm from '@/pages/order/UserInfoForm.tsx'
+import { Typography } from 'antd'
+
+const { Title } = Typography
 
 export const UserInfo = () => {
   const currentUser = useSelector((state: RootState) => state.user.currentUser)
   const navigate = useNavigate()
   const { email } = currentUser || {}
+  const [form] = Form.useForm()
 
   useEffect(() => {
     const isProd = process.env.NODE_ENV === 'production'
@@ -19,15 +24,19 @@ export const UserInfo = () => {
   return (
     <Flex gap="40px">
       <Col className="grow">
-        <Card title="修改密碼" className="p-10">
-          <div>電子信箱：</div>
-          <div>{email}</div>
-          <div>密碼：</div>
-          <div>************</div>
+        <Card className="px-8 pb-8">
+          <Title level={2}>修改密碼</Title>
+          <Title level={5}>電子信箱：</Title>
+          <Title level={5}>{email}</Title>
+          <Title level={5}>密碼：</Title>
+          <Title level={5}>************</Title>
         </Card>
       </Col>
       <Col className="grow-2">
-        <Card title="個人資料"></Card>
+        <Card className="px-8 pb-8">
+          <Title level={2}>個人資料</Title>
+          <UserInfoForm form={form} />
+        </Card>
       </Col>
     </Flex>
   )
